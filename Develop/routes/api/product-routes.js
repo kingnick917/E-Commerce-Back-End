dd6bdab2392f37851a8adcada506743a79608620
unknown <nmukhtar2001@gmail.com>
2023-04-13 22:37:46 -0700

    did some routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,16 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include:[Category,Tag]
+    });
+    res.status(200).json(productsData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+
+  }
 });
 
+
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    console.log(req.params.id)
+    const productsData = await Product.findByPk(req.params.id, {
+      include: [Category,Tag]
+    });
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+  
+ 
 });
+
 
 // create new product
 router.post('/', (req, res) => {
